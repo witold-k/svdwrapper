4ba635f307ad9e199668f2d23e0def0584a894d3
witold <witold@home>
2026-09-19 14:39:16 +0200

    Dosome cleanup/remove unused parts

--- build.rs
+++ build.rs
@@ -11,12 +11,10 @@ fn cpu() {
 #[cfg(feature = "cuda")]
 fn cuda() {
     use std::env;
-    use std::path::PathBuf;
     // 2. Dynamic Discovery of CUDA installation path
     let cuda_home = env::var("CUDA_HOME")
     .or_else(|_| env::var("CUDA_PATH"))
     .unwrap_or_else(|_| "/usr/local/cuda".to_string());
-    let cuda_include_path = format!("{}/include", cuda_home);
     println!("cargo:rerun-if-env-changed=CUDA_HOME");
     println!("cargo:rerun-if-env-changed=CUDA_PATH");
     println!("cargo:rustc-link-lib=cusolver");
@@ -24,26 +22,6 @@ fn cuda() {
     println!("cargo:rustc-link-search=native=/usr/lib");
     println!("cargo:rustc-link-search=native={}/lib64", cuda_home);
 
-    let builder = bindgen::Builder::default()
-        .header(format!("{}/cuda.h", cuda_include_path))
-        .clang_arg(format!("-I{}", cuda_include_path))
-        // Force bindgen to respect standard C booleans properly
-        .clang_arg("-include")
-        .clang_arg("stdbool.h")
-        // Use Rust-friendly enums instead of raw integers where possible
-        .rustified_enum(".*")
-        // Enforce the modern, updated naming convention (allowlist over whitelist)
-        .allowlist_function("cu.*")
-        .allowlist_function("cusolver.*");
-
-    let bindings = builder
-        .generate()
-        .expect("Unable to generate dynamic CUDA/MAGMA bindings");
-
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    bindings
-        .write_to_file(out_path.join("magma_bindings.rs"))
-        .expect("Couldn't write bindings to target OUT_DIR!");
 }
 
 #[cfg(feature = "julia")]
